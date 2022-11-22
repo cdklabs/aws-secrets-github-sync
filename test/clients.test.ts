217ebcd3b91f6a5cc2fcae12a9db6aa476ac3de4
Ryan Parker <parkerzr@amazon.com>
2022-11-22 09:17:11 -0800

    grammar!

--- test/clients.test.ts
+++ test/clients.test.ts
@@ -13,25 +13,25 @@ afterEach(() => {
   jest.resetAllMocks();
 });
 
-test('throws error when, getRepositoryName() sub process fails', async () => {
+test('throws error, when getRepositoryName() sub process fails', async () => {
   expect(
     () => clients.getRepositoryName(),
   ).toThrowError(/Failed to get repository name: .*/);
 });
 
-test('throws error when, storeSecret() sub process fails', async () => {
+test('throws error, when storeSecret() sub process fails', async () => {
   expect(
     () => clients.storeSecret('repo', 'name', 'value'),
   ).toThrowError(/Failed to store secret 'name' in repository 'repo': .*/);
 });
 
-test('throws error when, listSecrets() sub process fails', async () => {
+test('throws error, when listSecrets() sub process fails', async () => {
   expect(
     () => clients.listSecrets('repo'),
   ).toThrowError(/Failed to list secrets in repository 'repo': .*/);
 });
 
-test('throws error when, removeSecret() sub process fails', async () => {
+test('throws error, when removeSecret() sub process fails', async () => {
   expect(
     () => clients.removeSecret('repo', 'key'),
   ).toThrowError(/Failed to remove secret 'key' from repository 'repo': .*/);
